50ac0fbde80dd5855304ed29bcfdc34bbd5ea3c3
Tobias Timpe <tobias.timpe@hunter.de>
2020-09-28 12:37:51 +0200

    Should expose non-functioning APC outlets

--- src/APCHomeAccessory.ts
+++ src/APCHomeAccessory.ts
@@ -15,6 +15,8 @@ import {
 } from "homebridge";
 
 import APCHomePlatform from './APCHomePlatform'
+import APCDevice from './APCDevice'
+import APCOutlet from './APCOutlet'
 
 export default class APCHomeAccessory {
 	private Service: any
@@ -30,19 +32,73 @@ export default class APCHomeAccessory {
 		private readonly accessory: PlatformAccessory,
 		private readonly config: any,
 		private readonly log: Logging,
-		private readonly deviceId: string
+		private device: APCDevice
 		) {
 		this.platform = platform;
 		this.log = platform.log;
 
-		this.deviceId = deviceId
+		this.device = device
 
 		this.Service = this.platform.api.hap.Service
 
 		this.Characteristic = this.platform.api.hap.Characteristic
 
+		this.device = device
 
+
+		this.accessory.getService(this.platform.Service.AccessoryInformation)!
+		.setCharacteristic(this.platform.Characteristic.Manufacturer, 'APC')
+		.setCharacteristic(this.platform.Characteristic.FirmwareRevision, 'FIRMWARE')
+		.setCharacteristic(this.platform.Characteristic.Model, this.device.model)
+		.setCharacteristic(this.platform.Characteristic.SerialNumber, this.device.dsn.toString())
+
+		for (let i=0; i < this.device.outlets.length; i++) {
+			const uuid = this.platform.api.hap.uuid.generate('homebridge-apc-home-' + this.device.outlets[i].id)
+			let service = this.accessory.addService(this.platform.Service.Outlet, this.device.outlets[i].name, uuid)
+   			service.getCharacteristic(this.Characteristic.On)
+        		.on('get', this.handleOnGet.bind(this))
+       		    .on('set', this.handleOnSet.bind(this));
+
+      		service.getCharacteristic(this.Characteristic.OutletInUse)
+        		.on('get', this.handleOutletInUseGet.bind(this));
+
+			this.enabledServices.push(service)
+		}
+		
 	}
 
+
+  /**
+   * Handle requests to get the current value of the "On" characteristic
+   */
+  handleOnGet(callback: CharacteristicGetCallback) {
+    this.log.debug('Triggered GET On');
+
+    // set this to a valid value for On
+    
+
+    callback(null, 1);
+  }
+
+  /**
+   * Handle requests to set the "On" characteristic
+   */
+  handleOnSet(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.log.debug('Triggered SET On:', value);
+
+    callback(null);
+  }
+
+  /**
+   * Handle requests to get the current value of the "Outlet In Use" characteristic
+   */
+  handleOutletInUseGet(callback: CharacteristicGetCallback) {
+    this.log.debug('Triggered GET OutletInUse');
+
+    // set this to a valid value for OutletInUse
+    const currentValue = 1;
+
+    callback(null, currentValue);
+  }
 
 }
--- src/APCHomePlatform.ts
+++ src/APCHomePlatform.ts
@@ -57,6 +57,22 @@ export default class APCHomePlatform implements DynamicPlatformPlugin {
        let apcDevices: APCDevice[] = await this.apcService.getDevices()
       // Now make platform accessories out of those devices, check if they already exist, etc.
 
+        for (var i=0; i<apcDevices.length; i++) {
+          const uuid = this.api.hap.uuid.generate('homebridge-apc-home' + apcDevices[i].dsn)
+
+          const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid)
+
+          if (existingAccessory) {
+            this.log.info('Restoring accessory')
+
+          } else {
+            let accessory = new this.api.platformAccessory(apcDevices[i].product_name, uuid)
+            let apcAccessory = new APCHomeAccessory(this, accessory, this.config, this.log, apcDevices[i])
+            this.log.info('Created new accessory with name', apcDevices[i].product_name)
+            this.api.registerPlatformAccessories(PLUGIN_NAME,PLATFORM_NAME,[accessory])
+          }
+
+        }
     }
   }
 
